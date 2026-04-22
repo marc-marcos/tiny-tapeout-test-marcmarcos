26fad68c81cd43f121b478255078a573265d94e8
Marc Marcos <marcmarcosmadruga@gmail.com>
2026-04-22 18:47:54 +0200

    simple xor tree

--- src/project.v
+++ src/project.v
@@ -17,11 +17,12 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+   assign uo_out = {{7{1'b0}}, (ui_in[0] ^ ui_in[1]) ^ (ui_in[2] ^ ui_in[3]) };
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0, ui_in[7:4], uio_in};
 
 endmodule
